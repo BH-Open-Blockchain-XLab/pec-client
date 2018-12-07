b95fb79b83691da0050f369d169adba4b593fc77
Tang Zongxun <tangzongxun@hotmail.com>
2018-12-07 22:12:04 +0800

    added signin function

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+  baseUrl:"http://192.168.1.225:9090/test",
+};
+
+export default config;
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,104 +1,67 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import LockIcon from '@material-ui/icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {replace} from 'connected-react-router';
+import api from '../jsonapi';
 
-import {withRouter} from 'react-router';
 
-const styles = theme => ({
-  layout: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-});
+class SignIn extends React.Component {
+  constructor(props){
+    super(props);
 
-function SignIn(props) {
-  const { classes } = props;
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <main className={classes.layout}>
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="username">Username</InputLabel>
-              <Input id="username" name="username" autoFocus />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input
-                name="password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={()=>{props.history.replace('/dashboard/');}}
-            >
+  async handleLogin(event){
+    event.preventDefault();
+
+    let res = await api.post('/login', {
+      username: '',
+      password: '',
+    });
+    if(res.message == 'success'){
+      this.props.login(res.token);
+    } else{
+      alert("Login failed");
+    }
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        <h1 class="text-center">
+          Sign in
+        </h1>
+        <div class="container">
+          <form 
+            class="column col-6 col-sm-12 col-mx-auto"
+            onSubmit={this.handleLogin}   
+          >
+            <div class="form-group">
+              <div class="form-label" for="username">Username</div>
+              <input class="form-input" type="text" id="username" placeholder="Username" />
+            </div>
+            <div class="form-group">
+              <div class="form-label" for="password">Password</div>
+              <input class="form-input" type="text" id="password" placeholder="password" />
+            </div>
+            <button class="btn" type="submit">
               Sign in
-            </Button>
+            </button>
           </form>
-        </Paper>
-      </main>
-    </React.Fragment>
-  );
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-SignIn.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+let dispatchMap = (dispatch) => bindActionCreators(
+  {
+    login: ()=>replace('/dashboard/'),
+  },
+  dispatch
+);
 
-export default withRouter(withStyles(styles)(SignIn));
+SignIn = connect(null, dispatchMap)(SignIn);
+export default SignIn;
